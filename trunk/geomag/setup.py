a08978f0f0403525c6e02f1463320783347984a7
cmweiss@gmail.com <cmweiss@gmail.com@c15cea22-fec8-11de-bbca-1111db1fd05b>
2014-12-16 22:17:03 +0000

    Update

--- trunk/geomag/setup.py
+++ trunk/geomag/setup.py
@@ -2,21 +2,23 @@ from distutils.core import setup
 setup(
     name = "geomag",
     packages = ["geomag"],
-    data_files = [('geomag', ('geomag/WMM.COF',))],
-    version = "0.9",
+    #data_files = [('geomag', ('geomag/WMM.COF',))],
+	package_data = {'geomag': ['WMM.COF','WMM2010.COF']},
+    version = "0.9.2015",
     description = "Magnetic variation/declination",
     author = "Christopher Weiss",
     author_email = "cmweiss@gmail.com",
-    url = "http://url.of.geomag/",
-    download_url = "http://url.of.geomag/download/geomag-0.9.tgz",
+    url = "http://geomag.googlecode.com/",
     keywords = ["magnetic", "variation", "declination"],
     classifiers = [
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: GIS",
         "Topic :: Utilities",
         ],
     long_description = """\
@@ -24,6 +26,6 @@ Magnetic variation/declination
 ------------------------------
 
 Calculates magnetic variation/declination for any latitude/longitude/altitude,
-for any date. Uses the NOAA National Geophysical Data Center, epoch 2010 data.
+for any date. Uses the NOAA National Geophysical Data Center, epoch 2015 data.
 """
 )
